1b97eb2eee381636047fb75b8bf7f4e58a6e9838
awais <aanisar@lakeheadu.ca>
2021-02-14 14:35:06 -0500

    GroceryItem setName and get methods complete

--- src/GroceryItem.java
+++ src/GroceryItem.java
@@ -4,8 +4,35 @@ public class GroceryItem {
     private String category;
 
     public GroceryItem(String name, double price, String category) {
-        this.name = name;
+        setName(name);
+        setPrice(price);
+        setCategory(category);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name.length() > 2)
+            this.name = name;
+        else
+            throw new IllegalArgumentException("You need to type more than two characters long.");
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
         this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
         this.category = category;
     }
 }
