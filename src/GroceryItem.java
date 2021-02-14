3d8fd7966897e363e910ceca4eed71419c4d092d
awais <aanisar@lakeheadu.ca>
2021-02-14 14:29:10 -0500

    GroceryItem instance variables and constructor complete

--- src/GroceryItem.java
+++ src/GroceryItem.java
@@ -1,2 +1,11 @@
 public class GroceryItem {
+    private String name;
+    private double price;
+    private String category;
+
+    public GroceryItem(String name, double price, String category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
 }
