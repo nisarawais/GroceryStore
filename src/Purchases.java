3a6b0f708f60b4335c1883768f6faa07a95c0f69
awais <aanisar@lakeheadu.ca>
2021-02-15 17:06:29 -0500

    Purchases instance variable and CONSTRUCTOR complete

--- src/Purchases.java
+++ src/Purchases.java
@@ -2,4 +2,8 @@ import java.util.ArrayList;
 
 public class Purchases {
     private ArrayList<GroceryItem> purchases;
+
+    public Purchases() {
+        this.purchases = new ArrayList<>();
+    }
 }
